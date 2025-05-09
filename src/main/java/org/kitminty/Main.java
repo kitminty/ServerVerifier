17747f63485aa97e8041700151bab48fbc6c8521
kitminty <electrodkitty@gmail.com>
2025-05-09 17:42:33 -0400

    Stopped annoying logs and stopped duplicates from happening

--- src/main/java/org/kitminty/Main.java
+++ src/main/java/org/kitminty/Main.java
@@ -2,29 +2,16 @@ package org.kitminty;
 
 //cats are cool
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.mongodb.*;
-import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoDatabase;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.bson.Document;
-import org.reactivestreams.Publisher;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoCollection;
-import com.mongodb.reactivestreams.client.MongoDatabase;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.model.CreateCollectionOptions;
-import com.mongodb.client.model.ValidationOptions;
-import reactor.core.publisher.Mono;
-
+import org.slf4j.LoggerFactory;
 
 public class Main {
     public static boolean devmode = true;
     public static Dotenv dotenv = Dotenv.configure().directory("src/main/resources/.env").load();
     public static String serverjsonlist = devmode ? dotenv.get("SERVERJSONLIST") : "";
-    public static String outputtxt = devmode ? dotenv.get("OUTPUTTXT") : "";
     public static String databaseuri = devmode ? dotenv.get("DATABASEURI") : "";
     public static ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
     public static MongoClientSettings settings = MongoClientSettings.builder()
@@ -34,15 +21,13 @@ public class Main {
     public static int chunksize = 20;
 
     public static void main(String[] args) {
+        ((Logger) LoggerFactory.getLogger("org.mongodb.driver")).setLevel(Level.WARN);
+        ((Logger) LoggerFactory.getLogger("reactor.util.Loggers")).setLevel(Level.WARN);
         if (!devmode) {
             for (String arg : args) {
                 if (arg.startsWith("--serverjsonlist=")) {
                     serverjsonlist = arg.substring("--serverjsonlist=".length());
                     System.out.println(serverjsonlist);
-                }
-                if (arg.startsWith("--outputtxt=")) {
-                    outputtxt = arg.substring("--outputtxt=".length());
-                    System.out.println(outputtxt);
                 }
                 if (arg.startsWith("--databaseuri=")) {
                     databaseuri = arg.substring("--databaseuri=".length());
--- src/main/java/org/kitminty/ServerScanner.java
+++ src/main/java/org/kitminty/ServerScanner.java
@@ -1,11 +1,8 @@
 package org.kitminty;
 
-import com.mongodb.MongoException;
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoCollection;
-import com.mongodb.reactivestreams.client.MongoDatabase;
+import com.mongodb.reactivestreams.client.*;
 import com.tekgator.queryminecraftserver.api.Protocol;
 import com.tekgator.queryminecraftserver.api.QueryStatus;
 import org.bson.Document;
@@ -18,49 +15,33 @@ public class ServerScanner extends Thread {
         List<Masscan> serverList = ServerJsonProcessor.parseonlyjson(ServerJsonProcessor.Chunking(Main.serverjsonlist, Main.chunksize).get(Integer.parseInt(Thread.currentThread().getName().substring(7))).toString());
         for (Masscan server : serverList) {
             String address = server.ip();
-            short port = server.ports().getFirst().port();
             try {
-                String Description = new QueryStatus.Builder(address)
+                String description = new QueryStatus.Builder(address)
                         .setProtocol(Protocol.TCP)
                         .build()
                         .getStatus()
                         .getDescription();
-                int Players = new QueryStatus.Builder(address)
+                int players = new QueryStatus.Builder(address)
                         .setProtocol(Protocol.TCP)
                         .build()
                         .getStatus()
                         .getPlayers()
                         .getOnlinePlayers();
 
-                System.out.println("\u001B[32m" + "Server " + address + ":" + port + " Is On" + "\u001B[0m");
-
-                //--- think this will work keep it close to this
-
-                Document document = new Document().append("ip", address).append("description", Description).append("players", Players); //make it get just players from the json
-
+                Document document = new Document().append("_id", address).append("description", description).append("players", players);
                 try (MongoClient mongoClient = MongoClients.create(Main.settings)) {
-                    try {
-                        MongoDatabase database = mongoClient.getDatabase("Servers");
-                        MongoCollection<Document> Serverlist = database.getCollection("Serverlist");
-
-                        Publisher<InsertOneResult> insertPublisher = Serverlist.insertOne(document);
-                        Mono.from(insertPublisher).block();
-                    } catch (MongoException e) {
-                        e.printStackTrace();
+                    MongoDatabase database = mongoClient.getDatabase("Servers");
+                    MongoCollection<Document> Serverlist = database.getCollection("Serverlist");
+                    Publisher<InsertOneResult> insertPublisher = Serverlist.insertOne(document);
+                    Mono.from(insertPublisher).block();
+                    System.out.println("\u001B[32m" + "Server " + address + " is on" + "\u001B[0m");
+                } catch (MongoWriteException e) {
+                    if (e.getCode() == 11000) {
+                        System.out.println("\u001B[38;5;11m" + "Server " + document.get("_id") + " is duplicate" + "\u001B[0m");
                     }
                 }
-
-                //---
-
-                /* current writer
-                BufferedWriter out = new BufferedWriter(new FileWriter(Main.outputtxt, true));
-                out.write("Server " + address + ":" + port + " Is On, Json: (" + json + ")");
-                out.newLine();
-                out.close();
-                */
-
             } catch (Exception ignored) {
-                System.out.println("\u001B[31m" + "Server " + address + ":" + port + " Is Off" + "\u001B[0m");
+                System.out.println("\u001B[31m" + "Server " + address + " is off" + "\u001B[0m");
             }
         }
     }
